e32af22c8764c3352d97f55f20e4f5a1bd29fa13
Chien Chung <chungchien@163.com>
2015-01-22 20:00:42 +0800

    修正了config.h.in和readline.c中的兩處bug

--- lib/readline.c
+++ lib/readline.c
@@ -14,12 +14,12 @@ static ssize_t my_read(int fd, char *ptr)
  again:
     if ((read_cnt = read(fd, read_buf, sizeof(read_buf))) < 0) {
       if (errno == EINTR)
-        goto again;
+	goto again;
       return -1;
     } else if (read_cnt == 0) {
       return 0;
     }
-    readptr = read_buf;
+    read_ptr = read_buf;
   }
 
   read_cnt--;
@@ -37,7 +37,7 @@ ssize_t readline(int fd, void *vptr, size_t maxlen)
     if ((rc = my_read(fd, &c)) == 1) {
       *ptr++ = c;
       if (c == '\n')
-        break;
+	break;
     } else if (rc == 0) {
       *ptr = 0;
       return n - 1;
